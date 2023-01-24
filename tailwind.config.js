ea6e09262dc36b659c16a25734f9d0967762c949
Emmet Moore <x21192090@student.ncirl.ie>
2023-01-24 14:53:43 +0100

    Added custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,22 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        spotify: {
+          primary: "#1DB954",
+          secondary: "#ffffff",
+          accent: "#1ed760",
+          neutral: "#181818",
+          "base-100": "#191414",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+    ],
+  },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
