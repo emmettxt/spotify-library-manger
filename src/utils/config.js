3a2e88d5b267c095c960ffb2daaa9ad7e86d4eb9
Emmet Moore <x21192090@student.ncirl.ie>
2023-02-22 16:47:09 +0100

    dev config

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,11 @@
 const client_id = "f2c13475309244509ad70ea9ff6f7423";
-const redirect_uri = "http://localhost:3000/redirect/";
+const redirect_uri =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000/redirect/"
+    : "";
 
-const scopes = "user-library-read user-read-private user-read-email playlist-modify-private";
+const scopes =
+  "user-library-read user-read-private user-read-email playlist-modify-private";
 const config = {
   client_id,
   redirect_uri,
